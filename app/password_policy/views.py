65f7f630ceedc1297bc990c4b1def30471934ecf
Saradhi Nimmagadda <nimmagadda_s@India.XoriantCorp.com>
2021-01-18 08:14:31 +0530

    adding model viewset for password policy

--- app/password_policy/views.py
+++ app/password_policy/views.py
@@ -2,12 +2,14 @@ from rest_framework import viewsets, mixins
 from core.models import PasswordPolicy
 from password_policy import serializers
 
-class passwordPolicyViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class passwordPolicyViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
     """manage password policy database"""
 
     queryset = PasswordPolicy.objects.all()
     serializer_class = serializers.PasswordPolicySerializer
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        if self.request.user.is_superuser:
+            return queryset
+        return queryset.filter(user=self.request.user)
 
