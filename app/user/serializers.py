9a023c3a97a08a96b40a60274cb0ef90edc3972a
Pravin Tandale <Pravin.Tandale@Xoriant.com>
2021-01-18 18:45:11 +0530

    Default password policy, while creating user.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model, password_validation
+from django.core import exceptions
 from rest_framework import serializers
+
+from core.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -25,3 +28,15 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
             user.save()
         return user
+
+    def validate(self, data):
+        user = User(**data)
+        password = data.get('password')
+        errors = dict()
+        try:
+            password_validation.validate_password(password=password, user=user)
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+        if errors:
+            raise serializers.ValidationError(errors)
+        return super().validate(data)
