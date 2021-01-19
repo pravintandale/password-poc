6d7fe80dd8d1c248f4be4ea9c10905c4570f03cd
Pravin Tandale <Pravin.Tandale@Xoriant.com>
2021-01-19 22:52:40 +0530

    Remove unique from password policy name.

--- app/core/models.py
+++ app/core/models.py
@@ -54,7 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 class PasswordPolicy(models.Model):
     """password policy model"""
 
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='password_policy')
     min_number = models.IntegerField(default=0)
     min_length = models.IntegerField(default=8)
